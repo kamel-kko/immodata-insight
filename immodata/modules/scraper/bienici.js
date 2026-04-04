020ab74931db9e81b4a86536a091f1873d5c9bc5
Kamel <ton-email@example.com>
2026-04-04 19:59:36 +0200

    Sauvegarde automatique

--- immodata/modules/scraper/bienici.js
+++ immodata/modules/scraper/bienici.js
@@ -8,9 +8,9 @@
 (function () {
   'use strict';
 
-  const log = window.__immodata.createLogger('SCRAPER:BIENICI');
-  const security = window.__immodata.security;
-  const ext = window.__immodata.extractors;
+  const log = self.__immodata.createLogger('SCRAPER:BIENICI');
+  const security = self.__immodata.security;
+  const ext = self.__immodata.extractors;
 
   const SEL = {
     prix: [
@@ -140,7 +140,7 @@
     return cards;
   }
 
-  window.__immodata.scrapers = window.__immodata.scrapers || {};
-  window.__immodata.scrapers.bienici = { extractAnnonceData, extractCardsData };
+  self.__immodata.scrapers = self.__immodata.scrapers || {};
+  self.__immodata.scrapers.bienici = { extractAnnonceData, extractCardsData };
 
 })();
